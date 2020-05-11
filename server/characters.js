734e4414db4bda7f893371f2f1f5501a35e22f14
aisuhua <1079087531@qq.com>
2020-05-11 15:12:05 +0800

    Fix invalid pathname in linux

--- server/characters.js
+++ server/characters.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Character = require('./Character');
+const Character = require('./character');
 
 router.get('/', async (req, res) => {
     try {
